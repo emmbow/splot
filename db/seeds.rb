e672e84cd143e6a0d0bbc9ed1ef1dbb5465b9635
Emma Bowen <emloubow@gmail.com>
2019-12-11 14:13:59 +0000

    index page updated design

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,11 +26,17 @@ Site.create(name: "Mudchute City Farm")
 puts "Created sites"
 
 puts "Creating plots"
-Plot.create(plot_number: "Sarah's plot", site_id: Site.where(name: "Mudchute City Farm").take!.id, image_url: "https://images.unsplash.com/photo-1534710961216-75c88202f43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80")
-Plot.create(plot_number: "Tim's plot", site_id: Site.where(name: "Mudchute City Farm").take!.id, image_url: "https://images.unsplash.com/photo-1558717738-12f5dde036a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
-Plot.create(plot_number: "Oliver's plot", site_id: Site.where(name: "Mudchute City Farm").take!.id, image_url: "https://images.unsplash.com/photo-1557207562-a3157143784e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
-Plot.create(plot_number: "Cicely's plot", site_id: Site.where(name: "Mudchute City Farm").take!.id, image_url: "https://images.unsplash.com/photo-1507484467459-0c01be16726e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
-Plot.create(plot_number: "William's plot", site_id: Site.where(name: "Mudchute City Farm").take!.id, image_url: "https://images.unsplash.com/photo-1548400659-500be8d0279a?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80")
-Plot.create(plot_number: "Valerie's plot", site_id: Site.where(name: "Mudchute City Farm").take!.id, image_url: "https://images.unsplash.com/photo-1545165311-508ed0c91361?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
+Plot.create(plot_number: "Sarah's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1534710961216-75c88202f43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80")
+Plot.create(plot_number: "Tim's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1558717738-12f5dde036a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
+Plot.create(plot_number: "Oliver's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1557207562-a3157143784e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
+Plot.create(plot_number: "Cicely's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1507484467459-0c01be16726e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
+Plot.create(plot_number: "William's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1548400659-500be8d0279a?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80")
+Plot.create(plot_number: "Valerie's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1545165311-508ed0c91361?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
+Plot.create(plot_number: "Cicely's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1507484467459-0c01be16726e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
+Plot.create(plot_number: "William's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1548400659-500be8d0279a?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80")
+Plot.create(plot_number: "Valerie's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1545165311-508ed0c91361?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
+Plot.create(plot_number: "Cicely's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1507484467459-0c01be16726e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
+Plot.create(plot_number: "William's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1548400659-500be8d0279a?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80")
+Plot.create(plot_number: "Valerie's plot", site_id: Site.all.sample.id, image_url: "https://images.unsplash.com/photo-1545165311-508ed0c91361?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
 
 puts "Created plots"
